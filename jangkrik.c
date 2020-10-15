99c4834f8264b966ddcb441b4628b41ab662519a
Muhammad Nurhafiz Sidiq <72913185+nurhafizsidiq@users.noreply.github.com>
2020-10-15 17:17:15 +0800

    Update jangkrik.c
    anggota kelompok
    - Naufal Yanu Kusdianto
    - Muhammad Nurhafiz Sidiq
    - Muhammad Naufal Fadhlurrahman Suriadireja
    - Widyamukti Aulia Rahman
    - Abdan Husein Al Habsi
    - Cokorda Agung Arisma Putra Pemayun
    - Hervi Nur Rahmandien

--- jangkrik.c
+++ jangkrik.c
@@ -6,7 +6,7 @@ int main()
     int x1, v1, x2, v2;
     printf("masukkan X1 V1 X2 V2... ");
     scanf("%d %d %d %d", &x1, &v1, &x2, &v2);
-    if((x2-x1)%(v1-v2)==0){
+    if((x2-x1)%(v1-v2)==0 && v1-v2>0){
         printf("YES");
     }else{
         printf("NO");
